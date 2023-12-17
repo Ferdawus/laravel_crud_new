05dc2b60cfb3d5d60fcdcb1018673f408afa5f25
Ferdawus <85152217+Ferdawus@users.noreply.github.com>
2023-12-17 15:09:07 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Dashboard</title>
+</head>
+<body class="">
+    <h1 class="text-center mt-3 font-serif">Laravel Sanctum Api Authentication </h1>
+    {{-- <div class="flex">
+        <aside class="bg-indigo-600  h-screen w-[300px]">
+            <div class="text-white font-serif p-[18px] ">
+                <h6 class="text-xl text-center">Compny Logo</h6>
+            </div>
+            <nav class="text-white flex flex-col space-y-4 p-4">
+                <a href="" class="">Dashboard</a>
+                <a href="">Customers</a>
+                <a href="">Sales</a>
+                <a href="">Product</a>
+                <a href="">Settings</a>
+            </nav>
+        </aside>
+        <nav class="bg-black w-full h-14 border  border-black">
+
+        </nav>
+
+
+
+    </div>
+    <div class="main-content">
+        <main>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt vero atque labore non. Consequatur, beatae libero vitae possimus similique voluptatibus inventore voluptates modi labore provident. Quam sint laborum praesentium!
+        </main>
+    </div> --}}
+
+</body>
+</html>
